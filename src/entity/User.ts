3c53ad4b80fa85d33a2982d289e5ee75fb15cfdc
Rafał Rudko <rafal.rudko@rb.com>
2021-01-03 20:22:48 +0100

    add new fields for User model and update table name

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,16 +1,32 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
-@Entity()
+export enum UserRole {
+  ADMIN = "admin",
+  MODERATOR = "moderator",
+  USER = "user",
+}
+
+@Entity({ name: 'users' })
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  mail: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    type: "enum",
+    enum: UserRole,
+    default: UserRole.USER,
+  })
+  role: UserRole;
 
   @Column()
   firstName: string;
 
   @Column()
   lastName: string;
-
-  @Column()
-  age: number;
 }
